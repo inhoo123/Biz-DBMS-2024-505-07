2edf979a42b422b9c068261246cf370cf2241298
inhoo123 <xkdns50@naver.com>
2024-07-11 11:14:28 +0900

    2

--- 관리자.sql
+++ 관리자.sql
@@ -5,3 +5,5 @@ SYSDBA 는 Oracle DBMS 의 매우 중요한 권한을 나타낸다.
 SYSDBA는 DB 를 생성하고, 제거하고,사용자를 생성하고,제거하고,여러가지 시스템관리를 할수 있는 권한이다
 따라서 SYS 사용자의 비밀번호는 잘 관리되어야 한다.
 */
+--Oracle DBMS 프로젝트를 처음 시작할때는 관리자로 접속하여 TableSpace 와 User 를 생성해줘야 한다.
+--TableSpace 는 C:\app\data 폴더에 생성할 것
