1233598f7d0a763ee3119be95fcc26a341469c07
inhoo123 <xkdns50@naver.com>
2024-07-11 14:39:47 +0900

    asdfasdf

--- 관리자.sql
+++ 관리자.sql
@@ -28,4 +28,42 @@ DATAFILE 'c:/app/data/student.dbf'
 SIZE 2M AUTOEXTEND ON NEXT 2K;
 
 -- 사용자 생성
+/*
+오라클 12c 이후 버전에서 user 를 생성할때 일반적인 문자열만으로 user 를 생성할수 없다
+사용자 이름 앞에 반드시 C## 이 부착된 형태로 user name 을 생성해야 한다.
+이 설정은 여러가지 불편함이 발생하고
+특히 java 라던가 다른 곳에서 DBMS 에 접속하는 프로그래밍을 수행할때는 문재를 일으킬 수 있다.
+*/
+CREATE USER C##user1 -- user1 이라는 이름으로 사용자를 생성한다 
+IDENTIFIED BY 1234; --비밀번호는 1234 로 설정한다
+DROP USER C##user1 CASCADE;
+
+/*
+오라클 12c 이후 버전에서 영문+숫자 형식의 일번적인 사용자 ID 를 설정하기 위하여
+시스템 설정을 변경한 후 명령을 실행한다
+*/
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
 CREATE USER user1 IDENTIFIED BY 1234;
+DROP USER user1 CASCADE;
+
+/*
+USER 를 생성할때
+앞에서 생성한 TableSpace 와 USER 계정을 연결해주어야만
+사용자 데이터가 새로 생성한 TableSpace 에 저장된다
+*/
+CREATE USER user1 IDENTIFIED BY 1234
+DEFAULT TABLESPACE student;
+
+-- 새로 생선된 user1 은 아무런 권한이 없어서, 로그인 조차 할수 없다
+-- user1 에게 여러가지 권한을 부여해야만 DBMS 에 접근하여 업무를 수행할수 있다.
+/*
+권한 부여 정책
+생성된 USER(user1)에게 로그인, 테이블생성, CRUD, 기타 여라가지 권한을 일일히 부여하는 것이 실무상 원칙
+학습하는 입장에서, 로컬에 저장된 데이터에 접근한다는 제한적인 환경에서 여러가지 권한을 일일히 부여하는 것은
+상당히 불편함과 어려움이 있다.
+
+여기서는 USER1 에게 DBA 권한을 부여할 것이다
+오라클에서 DBA 권한 SYSDBA 보다 조금 제한된 권한이며,로그인,테이블생성 제거,CRUD 등의 업무를
+모두 수행할 수 있는 권한이다.
+*/
+GRANT DBA TO user1;
--- 오라클DBMS.md
+++ 오라클DBMS.md
@@ -6,3 +6,13 @@
   TableSpace 라는 것을 사용한다.
 - 오라클의 TableSpace는 접속한
   사용자 관리하는 파일의 개념으로 본다.
+  ## TableSpace
+- 오라클은 데이터저장소의 물리적 명칭을 TableSpace 라고 한다.
+- TableSpace 는 파일형식으로 구성이 되며, 오라클 DBMS
+ 소프트웨어에 의해 관리된다.
+- 오라클 DBMS 소프트웨어를 처음 설치하면 System
+TableSpace 만 있는 상태이다.
+- 여기에 만약 Table 등을 생성하면, 모든 데이터가 System Data
+ 영역에 저장이 되고, 보안 관리 측면에서 좋지 않은 결과를
+ 낼수있다.
+- 오라클에서는 프로젝트를 수행할때
